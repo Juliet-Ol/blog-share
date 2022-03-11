bea5dec193bd9b480539608438fde049e14690e6
Juliet <agutu@gmail.com>
2022-03-11 17:00:33 +0300

    index: route the html heading

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+
+    
+    message = 'Hello World'
+    return render_template('index.html',message = message)
