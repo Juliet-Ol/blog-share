bf5890c89450201723198b6444d40059302d7e99
Juliet <agutu@gmail.com>
2022-03-12 19:39:37 +0300

    create: create and and login and registraion fields

---
+++ app/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+class LoginForm(FlaskForm):
+
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField ('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm password',validators =[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')     
