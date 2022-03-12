c34f08a2bd4191c602c1535308263a359676bc7d
Juliet <agutu@gmail.com>
2022-03-12 19:44:22 +0300

    Route: route login and registration form

--- app/views.py
+++ app/views.py
@@ -1,10 +1,28 @@
-from flask import render_template
-from app import app
+from flask import render_template, redirect,url_for, flash
+from app import app, forms
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user
+
 
 # Views
 @app.route('/')
+@app.route('/index')
 def index():
 
+    return render_template ('index.html',title = 'home')
+
+@app.route('/login', methods = ['GET', 'POST'])    
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)      
     
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+
+@app.route('/register', methods = ['GET', 'POST'])    
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)       
+
+
+
+
+      
