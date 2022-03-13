f1f90400a848bafc44c9dd936cc753f22f66562a
Juliet <agutu@gmail.com>
2022-03-13 21:09:50 +0300

    create config file in app folder

--- app/config.py
+++ app/config.py
@@ -1,26 +1,17 @@
+from instance.config import RANDOM_QUOTES_URL
+
 class Config:
-   
-   
+
     pass
 
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
 
     DEBUG = True
