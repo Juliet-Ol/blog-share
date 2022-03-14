9ca6a835136ed38e600bd62c0f58e1ef686f2511
Juliet <agutu@gmail.com>
2022-03-14 08:55:52 +0300

    Route: route random quotes

--- app/views.py
+++ app/views.py
@@ -2,14 +2,21 @@ from flask import render_template, redirect,url_for, flash
 from app import app, forms
 from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user
+from .request import get_random_quotes
 
 
 # Views
 @app.route('/')
-@app.route('/index')
 def index():
 
-    return render_template ('index.html',title = 'home')
+    # Getting random quotes
+
+    random_quotes = get_random_quotes()
+    print(random_quotes)
+
+   
+    title = 'Home - Quotes and more Quotes'
+    return render_template('index.html', title = title, random = random_quotes )
 
 @app.route('/login', methods = ['GET', 'POST'])    
 def login():
