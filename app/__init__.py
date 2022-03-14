6db5fb2ddd8f9a08e266291f0293dc1b3ed384a5
Juliet <agutu@gmail.com>
2022-03-14 08:48:41 +0300

    Instance: create Instance folder and add secret key to be able to access quotes

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,16 @@
 from ensurepip import bootstrap
 from flask import Flask
+from app.config import DevConfig
 from config import Config
 from flask_bootstrap import Bootstrap
 
+# Initializing application
+app = Flask(__name__,instance_relative_config = True)
 
-
-#initializing application
-app = Flask(__name__)
-app.config.from_object(Config)
-
+# Setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 bootstrap = Bootstrap(app)
 
-
-from app import views, models
+from app import views
