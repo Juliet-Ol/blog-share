75029db7b38b51ed44faa74b16309f3bc665f5cf
Juliet <agutu@gmail.com>
2022-03-16 22:18:11 +0300

    Added Logout

--- app/views.py
+++ app/views.py
@@ -3,13 +3,15 @@ from flask import render_template, redirect,url_for, flash
 from app import app, forms, db
 from app.models import  User, Quote
 from app.forms import LoginForm, RegistrationForm, QuoteForm
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, login_required, logout_user
 from .request import get_random_quotes
 
 # Views
 
 @app.route('/')
 def index():
+    db.session.query(User).delete()
+    db.session.commit()
 
     # Getting random quotes
 
@@ -27,7 +29,7 @@ def login():
         if user is not None and user.check_password(form.password.data):
             login_user(user,form.remember.data)
             return redirect('/')
-    return render_template('login.html', title='Login', form=form)      
+    return render_template('login.html', title='Login', form=form, user=current_user)      
     
 
 @app.route('/register', methods = ['GET', 'POST'])    
@@ -42,10 +44,15 @@ def register():
         flash(f'Account created for {user.username}')
         return redirect(url_for('login'))
         
-    return render_template('register.html', title='Register', form=form)    
+    return render_template('register.html', title='Register', form=form, user=current_user)    
 
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
 
-@app.route('/quote', methods = ['GET', 'POST'])    
+@app.route('/quote', methods = ['GET', 'POST'])
 def quote():
     quoteForm = QuoteForm()
     quotes = Quote.query.all()
